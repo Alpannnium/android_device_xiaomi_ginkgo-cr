d3e825c20c14a8deb1dd58e898e7f8d7be421f98
AlpannVerse <revanzakindar2008@gmail.com>
2026-02-26 09:37:16 +0700

    Update lineage_ginkgo.mk

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -35,8 +35,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210905.001 7511028 release-keys
+BUILD_FINGERPRINT := xiaomi/ginkgo_eea/ginkgo:11/RKQ1.201004.002/V12.5.12.0.RCOEUXM:user/release-keys
+BUILD_DESCRIPTION := ginkgo-user 11 RKQ1.201004.002 V12.5.12.0.RCOEUXM release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
